436deb165d09342703eca6abe791ac1005be1a9b
Mateusz <matinowak24@gmail.com>
2023-04-05 14:56:41 +0200

    Dodaj pliki projektów.

---
+++ PT_laby_2/Create_dialog.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PT_laby_2
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Create_dialog.xaml
+    /// </summary>
+    public partial class Create_dialog : Window
+    {
+        public bool file_created = false;
+        private string dir;
+        private bool isDir = true;
+        private string full_dir;
+
+
+        public Create_dialog(string directory)
+        {
+            dir = directory;
+            InitializeComponent();
+        }
+
+        private void create_butt(object sender, RoutedEventArgs e)
+        {
+            full_dir = dir + "\\" + Name_str.Text;
+            if (isDir) 
+            {
+                Directory.CreateDirectory(full_dir);
+                SetRAHSAtributes(full_dir);
+                file_created=true;
+            }
+            else if (Regex.IsMatch(Name_str.Text, "^[A-Za-z0-9_~-]{1,8}\\.(txt|php|html)$")) 
+            {
+                File.Create(full_dir);
+                SetRAHSAtributes(full_dir);                
+                file_created = true;
+            }
+            else
+            {
+                MessageBox.Show("something is wrong with your file name");
+            }
+
+            if (file_created)
+            {
+                this.Close();
+            }
+        }
+
+        private void SetRAHSAtributes(string dir)
+        {
+            FileAttributes atr = FileAttributes.Normal;
+            if((bool)Readable_bool.IsChecked)
+            {
+                atr |= FileAttributes.ReadOnly;
+            }
+            if((bool)Archive_bool.IsChecked) 
+            { 
+                atr |= FileAttributes.Archive;
+            }
+            if((bool)System_bool.IsChecked)
+            {
+                atr |= FileAttributes.System;
+            }
+            if((bool)Hidden_bool.IsChecked) 
+            {
+                atr |= FileAttributes.Hidden;
+            }
+
+            File.SetAttributes(dir, atr);
+        }
+
+        private void exit_butt(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        public bool IsDir() { return isDir; }
+        private void SetDir(object sender, RoutedEventArgs e) { isDir = true; }
+        private void SetFile(object sender, RoutedEventArgs e) { isDir = false; }
+        public string path() { return full_dir; }    
+        public string name() { return Name_str.Text; }
+    }
+}
---
+++ PT_laby_2/MainWindow.xaml.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PT_laby_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void ProcessDirectory(ItemsControl root, String targetDirectory)
+        {
+            DirectoryInfo d = new DirectoryInfo(targetDirectory);
+            var item = new TreeViewItem();
+            item.Tag = targetDirectory;
+            item.Header = d.Name;
+            item.ContextMenu = new ContextMenu();
+            MenuItem create = new MenuItem() { Header = "Create" };
+            create.Click += Create_func;
+            MenuItem delete = new MenuItem() { Header = "Delete" };
+            delete.Click += Delete_dir_func;
+            item.ContextMenu.Items.Add(create);
+            item.ContextMenu.Items.Add(delete);
+            root.Items.Add(item);
+
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+                ProcessFile(item, fileName);
+
+
+            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach (string subdirectory in subdirectoryEntries)
+            {                
+                ProcessDirectory(item, subdirectory);
+            }
+
+        }
+
+        void Create_func(object sender, RoutedEventArgs e) {
+            TreeViewItem clickedDir = (TreeViewItem)((ContextMenu)((MenuItem)sender).Parent).PlacementTarget;
+            string dir = (string)clickedDir.Tag;
+            var dlg = new Create_dialog(dir); 
+            dlg.ShowDialog();
+            if (!dlg.file_created)
+            {
+                return;
+            }
+            if (dlg.IsDir())
+            {
+                Add_dir(dlg.path(), dlg.name(), clickedDir);
+            }
+            else
+            {
+                Add_file(dlg.path(), dlg.name(), clickedDir);
+            }
+        }
+
+        void Add_dir(string path, string name, ItemsControl root)
+        {
+            var item = new TreeViewItem();
+            item.Tag = path;
+            item.Header = name;
+            item.ContextMenu = new ContextMenu();
+            MenuItem create = new MenuItem() { Header = "Create" };
+            create.Click += Create_func;
+            MenuItem delete = new MenuItem() { Header = "Delete" };
+            delete.Click += Delete_dir_func;
+            item.ContextMenu.Items.Add(create);
+            item.ContextMenu.Items.Add(delete);
+            root.Items.Add(item);
+        }
+
+        void Add_file(string path, string name, ItemsControl root)
+        {
+            var item = new TreeViewItem();
+            item.Tag = path;
+            item.Header = name;
+            item.ContextMenu = new ContextMenu();
+            MenuItem open = new MenuItem() { Header = "open" };
+            open.Click += Show_file_func;
+            MenuItem delete = new MenuItem() { Header = "Delete" };
+            delete.Click += Delete_file_func;
+            item.ContextMenu.Items.Add(open);
+            item.ContextMenu.Items.Add(delete);
+            root.Items.Add(item);
+        }
+
+        void Show_file_func(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem clickedFile = (TreeViewItem)((ContextMenu)((MenuItem)sender).Parent).PlacementTarget;
+            string text = File.ReadAllText(clickedFile.Tag as string);
+            TextBlock1.Text = text;
+        }
+
+        void Delete_file_func(object sender, RoutedEventArgs e)
+        {
+            RemoveFromTree(sender, e);
+            TreeViewItem chosen_object = (TreeViewItem)((ContextMenu)((MenuItem)sender).Parent).PlacementTarget;
+            FileInfo f = new FileInfo(chosen_object.Tag as string);
+            f.Attributes = FileAttributes.Normal;
+            f.Delete();
+        }
+
+        void RemoveFromTree(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem chosen_object = (TreeViewItem)((ContextMenu)((MenuItem)sender).Parent).PlacementTarget;
+            TreeViewItem parent = (TreeViewItem)chosen_object.Parent;
+            parent.Items.Remove(chosen_object);
+        }
+        void Delete_dir_func(object sender, RoutedEventArgs e)
+        {
+            RemoveFromTree(sender, e);
+            TreeViewItem chosen_object = (TreeViewItem)((ContextMenu)((MenuItem)sender).Parent).PlacementTarget;
+            DirectoryInfo d = new DirectoryInfo(chosen_object.Tag as string);
+            d.DeleteAll();
+        }
+
+        public void ProcessFile(ItemsControl root, String path)
+        {
+            FileInfo f = new FileInfo(path);
+            var item = new TreeViewItem() { Tag = path };
+            item.Header = f.Name;
+            item.ContextMenu = new ContextMenu();
+            MenuItem open = new MenuItem() { Header = "open" };
+            open.Click += Show_file_func;
+            MenuItem delete = new MenuItem() { Header = "delete" };
+            delete.Click += Delete_file_func;
+            item.ContextMenu.Items.Add(open);
+            item.ContextMenu.Items.Add(delete);
+            root.Items.Add(item);
+            
+        }
+
+        private void Open_file(object sender, RoutedEventArgs e)
+        {
+            var dlg = new FolderBrowserDialog() { Description = "Select directory to open" };
+            dlg.ShowDialog();
+            ProcessDirectory(triview, dlg.SelectedPath);
+        }
+
+        private void Exit_program(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void About(object sender, RoutedEventArgs e)
+        {
+            var dlg = new About();
+            dlg.ShowDialog();
+        }
+
+        private void RAHS_attr(object sender, RoutedEventArgs e)
+        {
+            FileInfo file = new FileInfo((string)((TreeViewItem)triview.SelectedItem).Tag);
+            RAHS_info.Text = file.RAHS_atribiute();
+        }
+
+    }
+
+
+
+    public static class MISC
+    {
+        public static void DeleteAll(this System.IO.DirectoryInfo d)
+        {
+            DirectoryInfo[] sub_d = d.GetDirectories();
+            FileInfo[] sub_f = d.GetFiles();   
+            foreach(FileInfo file in sub_f)
+            {
+                file.Attributes = FileAttributes.Normal;
+                file.Delete();
+            }
+            foreach(DirectoryInfo dir in sub_d)
+            {
+                dir.DeleteAll();
+            }
+            d.Attributes = FileAttributes.Normal;
+            d.Delete();
+        }
+
+
+
+
+        public static string RAHS_atribiute(this FileSystemInfo info)
+        {
+            String rahs = "";
+            if (info.Attributes.HasFlag(FileAttributes.ReadOnly))
+                rahs += "r";
+            else
+                rahs += "-";
+            if (info.Attributes.HasFlag(FileAttributes.Archive))
+                rahs += "a";
+            else
+                rahs += "-";
+            if (info.Attributes.HasFlag(FileAttributes.Hidden))
+                rahs += "h";
+            else
+                rahs += "-";
+            if (info.Attributes.HasFlag(FileAttributes.System))
+                rahs += "s";
+            else
+                rahs += "-";
+
+            return rahs;
+        }
+    }
+}
